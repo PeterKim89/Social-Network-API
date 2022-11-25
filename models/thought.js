67e7446dbb8d6bde8a657173103a1d20fbce0c04
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-11-25 15:23:01 -0500

    createdAt date assigned

--- models/thought.js
+++ models/thought.js
@@ -1,20 +1,24 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const thoughtSchema = new Schema ({
-    thoughtText: {
-        type: String,
-        required: true,
-        // between 1 and 280 chars
-        min_length: 1,
-        max_length: 280,
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            // between 1 and 280 chars
+            min_length: 1,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {},
+        reactions: {},
     },
-    createdAt: {
-
-    },
-    username: {
-
-    },
-    reactions: {
-
+    {
+        toJSON: {
+            getters: true,
+        }
     }
-})
+);
