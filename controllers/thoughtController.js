5ffbb9e372115fe340f42d6246602f2ebc398ee6
Peter Kim <peter.kim@uconn.edu>
2022-12-15 02:14:35 -0500

    getThoughts and getSingleThought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,9 +1,29 @@
 const { Thought, User } = require("../models");
 
 const thoughtController = {
-	getThoughts(req, res) {},
+	getThoughts(req, res) {
+		Thought.find()
+			.then((courses) => {
+				res.json(courses);
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	},
 
-	getSingleThought(req, res) {},
+	getSingleThought(req, res) {
+		Thought.findOne({ _id: req.params.id })
+			.then((thought) => {
+				if (!thought) {
+					res.status(404).json({ message: "No thought with that ID" });
+				} else {
+					res.json(thought);
+				}
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	},
 
 	createThought(req, res) {},
 
