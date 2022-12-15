02dbd4c7355be96edf6fd0cb52e306d5c4298977
Peter Kim <peter.kim@uconn.edu>
2022-12-15 00:43:43 -0500

    getSingleUser and createUser methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,16 +8,42 @@ const userController = {
 				select: "-__v",
 			})
 			.select("-__v")
-			.then((userDbData) => res.json(userDbData))
+			.then((userDbData) => {
+				res.json(userDbData);
+			})
 			.catch((err) => {
 				console.log(err);
 				res.status(500).json(err);
 			});
 	},
 
-	getSingleUser(req, res) {},
+	getSingleUser(req, res) {
+		User.findOne({ _id: req.params.userId })
+			.select("-__v")
+			.populate("friends")
+			.populate("thoughts")
+			.then((userDbData) => {
+				if (!userDbData) {
+					return res.status(404).json({ message: "No user with this id!" });
+				} else {
+					res.json(userDbData);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				res.status(500).json(err);
+			});
+	},
 
-	createUser(req, res) {},
+	createUser(req, res) {
+		User.create(req.body)
+			.then((userDbData) => {
+				res.json(userDbData);
+			})
+			.catch((err) => {
+				res.status(400).json(err);
+			});
+	},
 
 	updateUser(req, res) {},
 
